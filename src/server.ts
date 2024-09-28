042b4bc4e89cd746016df684cb5d1e00d425231b
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-09-28 16:47:41 -0300

    connect to mongodb database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db";
+
+// Load .env file
+dotenv.config();
+
+// Connect to database
+connectDB();
 
 const app = express();
 
