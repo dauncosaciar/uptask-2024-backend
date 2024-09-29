ca661a783f5e8a4a9f95b46ac3c0ff5c696483e5
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-09-28 21:26:45 -0300

    add createProject method

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,17 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
 
-// Load .env file
+// Allow reading of .env files
 dotenv.config();
 
 // Connect to database
 connectDB();
 
+// Create express application
 const app = express();
+
+// Enable read forms data
+app.use(express.json());
 
 // Routes
 app.use("/api/projects", projectRoutes);
