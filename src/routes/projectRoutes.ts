c5918d64d40a74a53e7de8416ca737e3d0dc306e
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-09-30 10:47:25 -0300

    add server validation for projects

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,28 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middlewares/validation";
 
 const router = Router();
 
-router.post("/", ProjectController.createProject);
+router.post(
+  "/",
+  body("projectName")
+    .trim()
+    .notEmpty()
+    .withMessage("El Nombre del Proyecto es obligatorio"),
+  body("clientName")
+    .trim()
+    .notEmpty()
+    .withMessage("El Nombre del Cliente es obligatorio"),
+  body("description")
+    .trim()
+    .notEmpty()
+    .withMessage("La Descripción del Proyecto es obligatoria"),
+  handleInputErrors,
+  ProjectController.createProject
+);
+
 router.get("/", ProjectController.getAllProjects);
 
 export default router;
