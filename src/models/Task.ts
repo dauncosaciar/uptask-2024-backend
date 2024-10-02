7289ea5fce5de903a92e5ba6ddad2a5e28c29fd3
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-10-02 17:24:07 -0300

    relating project and task models

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,22 +1,30 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema, Types } from "mongoose";
 
 export interface ITask extends Document {
   name: string;
   description: string;
+  project: Types.ObjectId;
 }
 
-export const TaskSchema: Schema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true
+export const TaskSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    project: {
+      type: Types.ObjectId,
+      ref: "Project"
+    }
   },
-  description: {
-    type: String,
-    trim: true,
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 const Task = mongoose.model<ITask>("Task", TaskSchema);
 
