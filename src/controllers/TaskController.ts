ffe9d4e49e3149609ed61b14943e4bff7a37a958
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-10-06 11:41:55 -0300

    add getTaskById method

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -24,4 +24,27 @@ export class TaskController {
       res.status(500).json({ error: "Hubo un error" });
     }
   };
+
+  static getTaskById = async (req: Request, res: Response) => {
+    try {
+      const { taskId } = req.params;
+      const task = await Task.findById(taskId);
+
+      if (!task) {
+        const error = new Error("Tarea no encontrada");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      if (task.project.toString() !== req.project.id) {
+        const error = new Error("La tarea no pertenece al proyecto");
+        res.status(400).json({ error: error.message });
+        return;
+      }
+
+      res.json(task);
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error " });
+    }
+  };
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -83,4 +83,10 @@ router.get(
   TaskController.getProjectTasks
 );
 
+router.get(
+  "/:projectId/tasks/:taskId",
+  validateProjectExists,
+  TaskController.getTaskById
+);
+
 export default router;
