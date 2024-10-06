46dc387cc38c4e4ac272b5fbd042c1d8fc521a3d
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-10-06 10:59:42 -0300

    populate tasks field on project

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -24,7 +24,7 @@ export class ProjectController {
   static getProjectById = async (req: Request, res: Response) => {
     try {
       const { id } = req.params;
-      const project = await Project.findById(id);
+      const project = await Project.findById(id).populate("tasks");
 
       if (!project) {
         const error = new Error("Proyecto no encontrado");
