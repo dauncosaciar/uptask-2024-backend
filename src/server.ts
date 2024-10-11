a00129b6f9026bc60b44f8581ad5ba714edbad81
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-10-11 12:22:49 -0300

    allow cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
 
@@ -11,6 +13,9 @@ connectDB();
 
 // Create express application
 const app = express();
+
+// Allow CORS
+app.use(cors(corsConfig));
 
 // Enable read forms data
 app.use(express.json());
