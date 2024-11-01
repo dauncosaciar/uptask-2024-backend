71df61db6ff6d06f46c50db4d920581bb210fbce
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-11-01 09:27:06 -0300

    create auth controller

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,26 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middlewares/validation";
+import { AuthController } from "../controllers/AuthController";
 
 const router = Router();
 
 // Routes for Authentication
-router.get("/", (req, res) => {
-  res.send("desde /api/auth");
-});
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("El nombre no puede ir vacío"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña es muy corta, mínimo 8 caracteres"),
+  body("passwordConfirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Las contraseñas no coinciden");
+    }
+    return true;
+  }),
+  body("email").isEmail().withMessage("Email no válido"),
+  handleInputErrors,
+  AuthController.createAccount
+);
 
 export default router;
