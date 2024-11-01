967c05e2ca84acad36c5a53817e8f0bd9ab2f898
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-11-01 17:22:58 -0300

    prevent duplicate users in createAccount

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,8 +5,19 @@ import { hashPassword } from "../utils/auth";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { email, password } = req.body;
+
+      // Prevent duplicate users
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        const error = new Error("El Usuario con este email ya está registrado");
+        res.status(409).json({ error: error.message });
+        return;
+      }
+
+      // Create user
       const user = new User(req.body);
-      const { password } = req.body;
 
       // Hash password
       user.password = await hashPassword(password);
