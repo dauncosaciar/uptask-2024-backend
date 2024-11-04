427ed7e1013093640c433a15e92a3b37deb4b69b
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-11-04 11:37:27 -0300

    generate 6 digits token for a new user

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
+import Token from "../models/Token";
 import { hashPassword } from "../utils/auth";
+import { generate6DigitsToken } from "../utils/token";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
@@ -22,7 +24,12 @@ export class AuthController {
       // Hash password
       user.password = await hashPassword(password);
 
-      await user.save();
+      // Generate token for the new user
+      const token = new Token();
+      token.token = generate6DigitsToken();
+      token.user = user.id;
+
+      await Promise.allSettled([user.save(), token.save()]);
       res.send("Cuenta creada, revisa tu email para confirmarla");
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
