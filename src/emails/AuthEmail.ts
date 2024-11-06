2959a4164f4805e542fbf5ce0eae73033eeca6c3
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-11-06 20:26:24 -0300

    change anchor href value to confirm account on frontend

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -16,7 +16,7 @@ export class AuthEmail {
       html: `
         <p>Hola, ${user.name}. Has creado tu cuenta en UpTask, ya casi está todo listo, sólo debes confirmar tu cuenta.</p>
         <p>Visita el siguiente enlace: </p>
-        <a href="#">Confirmar cuenta</a>
+        <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
         <p>E ingresa el código: <b>${user.token}</b></p>
         <p>Este token expira en 10 minutos.</p>
       `
