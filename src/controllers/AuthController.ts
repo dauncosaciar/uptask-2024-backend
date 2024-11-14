c8a370d40181e3e568bc428b1c00662584250588
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-11-14 09:30:54 -0300

    add auth forgot password code endpoint

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -151,4 +151,35 @@ export class AuthController {
       res.status(500).json({ error: "Hubo un error" });
     }
   };
+
+  static forgotPassword = async (req: Request, res: Response) => {
+    try {
+      const { email } = req.body;
+      const user = await User.findOne({ email });
+
+      // Check if user exists
+      if (!user) {
+        const error = new Error("El Usuario con este email no está registrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      // Generate token for the new user
+      const token = new Token();
+      token.token = generate6DigitsToken();
+      token.user = user.id;
+      await token.save();
+
+      // Send email
+      AuthEmail.sendPasswordResetToken({
+        email: user.email,
+        name: user.name,
+        token: token.token
+      });
+
+      res.send("Revisa tu email para instrucciones");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
 }
--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -24,4 +24,22 @@ export class AuthEmail {
 
     console.log("Mensaje enviado", info.messageId);
   };
+
+  static sendPasswordResetToken = async (user: IEmail) => {
+    const info = await transporter.sendMail({
+      from: "UpTask <admin@uptask.com>",
+      to: user.email,
+      subject: "UpTask - Reestablece tu contraseña",
+      text: "UpTask - Reestablece tu contraseña",
+      html: `
+        <p>Hola, ${user.name}. Has solicitado reestablecer tu contraseña.</p>
+        <p>Visita el siguiente enlace: </p>
+        <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer password</a>
+        <p>E ingresa el código: <b>${user.token}</b></p>
+        <p>Este token expira en 10 minutos.</p>
+      `
+    });
+
+    console.log("Mensaje enviado", info.messageId);
+  };
 }
