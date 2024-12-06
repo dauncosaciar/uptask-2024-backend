e215916563114f8f745075d9b8da2651ef36f685
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-12-06 15:24:34 -0300

    show a task history changes

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -28,7 +28,7 @@ export class TaskController {
   static getTaskById = async (req: Request, res: Response) => {
     try {
       const task = await Task.findById(req.task.id).populate({
-        path: "completedBy",
+        path: "completedBy.user",
         select: "id name email"
       });
       res.json(task);
@@ -66,11 +66,12 @@ export class TaskController {
       const { status } = req.body;
       req.task.status = status;
 
-      if (status === "pending") {
-        req.task.completedBy = null;
-      } else {
-        req.task.completedBy = req.user.id;
-      }
+      const data = {
+        user: req.user.id,
+        status
+      };
+
+      req.task.completedBy.push(data);
       await req.task.save();
       res.send("Tarea actualizada correctamente");
     } catch (error) {
