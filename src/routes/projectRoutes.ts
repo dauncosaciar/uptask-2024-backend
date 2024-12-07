51b6b5ee6893a64cdd223902eabb15ba4ff461f8
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-12-07 16:01:25 -0300

    create note controller

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -11,6 +11,7 @@ import {
 import { ProjectController } from "../controllers/ProjectController";
 import { TaskController } from "../controllers/TaskController";
 import { TeamController } from "../controllers/TeamController";
+import { NoteController } from "../controllers/NoteController";
 
 const router = Router();
 
@@ -154,6 +155,16 @@ router.delete(
   param("userId").isMongoId().withMessage("ID no válido"),
   handleInputErrors,
   TeamController.removeMemberById
+);
+
+// Routes for Notes
+router.post(
+  "/:projectId/tasks/:taskId/notes",
+  body("content")
+    .notEmpty()
+    .withMessage("El Contenido de la Nota es obligatorio"),
+  handleInputErrors,
+  NoteController.createNote
 );
 
 export default router;
